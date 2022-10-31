f15e7d4bd1694d8023c3e2c102fe02665d14b5cc
aldo90orozco <aldo90.orozco@gmail.com>
2022-10-31 15:12:28 -0300

    primeros ejercicios

---
+++ ejercicios1.js
@@ -0,0 +1,21 @@
+/**Funcion para sumar numeros */
+let sumaNumeros= (numUno,num2,num3)=>{
+    return numUno+num2+num3;
+}
+
+console.log(sumaNumeros(2,4,6));
+
+/***********Objeto************ */
+let coche={cantidadPuertas:2}
+
+
+function agregarPuerta(cantidadAgregar){
+    return(coche.cantidadPuertas+cantidadAgregar)
+}
+
+
+
+console.log(agregarPuerta(1));
+
+
+
