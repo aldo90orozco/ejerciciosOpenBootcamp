23973391e511b44022f5e22d4e3bc90ac8b5de6e
aldo90orozco <70987777+aldo90orozco@users.noreply.github.com>
2022-11-02 01:33:48 -0300

    Add files via upload

---
+++ PrimerProyectoNode/primerEjercicio.js
@@ -0,0 +1,14 @@
+const miNombre='Aldo';
+let edad=32;
+let eresDesarrollador= false;
+const fechaNacimiento=new Date(1990, 2, 01);
+console.log(fechaNacimiento);
+
+const libroFavorito={
+    titulo:"El hobbit",
+    auto:'J.R.Tolkien',
+    fecha:'',
+    url:'',
+}
+
+console.log(libroFavorito.titulo);
